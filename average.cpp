da6757f43f927642fce2169ea1c1f3ca96d24b8d
NatchananP <natchananpinky@gmail.com>
2024-02-27 14:04:30 +0700

    average.cpp complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstdlib>
 
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "Please input numbers to find average." << std::endl;
+        return 0;
+    }
+
+    int count = argc - 1;
+    double sum = 0;
+
+    for (int i = 1; i < argc; ++i) {
+        sum += std::atof(argv[i]);
+    }
+
+    double average = sum / count;
+
+    std::cout << "---------------------------------" << std::endl;
+    std::cout << "Average of " << count << " numbers = " << average << std::endl;
+    std::cout << "---------------------------------" << std::endl;
+
+    return 0;
+}
